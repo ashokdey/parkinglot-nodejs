d1d1c570bffd8ba7efb7f78c8fb1a6e87c681493
ashokdey <ashokdey100@gmail.com>
2020-12-03 18:00:23 +0530

    up: packages & cleanup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import path from 'path';
 import logger from 'morgan';
-import bodyParser from 'body-parser';
 import routes from './routes';
 
 const app = express();
@@ -12,10 +11,10 @@ app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev', {
-  skip: () => app.get('env') === 'test'
+	skip: () => app.get('env') === 'test'
 }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
@@ -23,18 +22,18 @@ app.use('/', routes);
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // Error handler
 app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  res
-    .status(err.status || 500)
-    .render('error', {
-      message: err.message
-    });
+	res
+		.status(err.status || 500)
+		.render('error', {
+			message: err.message
+		});
 });
 
 export default app;
