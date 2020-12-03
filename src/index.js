7cf711dbdedeb5dfa77d96acf64ae77d975c1b57
ashokdey <ashokdey100@gmail.com>
2020-12-03 18:03:34 +0530

    add: env setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import app from './app';
+(async () => {
+	require('dotenv').config();
+	const app = require('./app').default;
 
-const { PORT = 8080 } = process.env;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
+	const { PORT = 8080 } = process.env;
+	// eslint-disable-next-line no-console
+	app.listen(PORT, async () => {
+		try {
+			// eslint-disable-next-line no-unused-vars
+			/**
+			 * Storage connections
+			 */
+			// eslint-disable-next-line no-console
+			console.info(`Listening on port ${PORT}...`);
+		} catch (err) {
+			// eslint-disable-next-line no-console
+			console.error(err);
+		}
+	});
+
+	process.on('unhandledRejection', (reason, p) => {
+		// eslint-disable-next-line no-console
+		console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+		// application specific logging, throwing an error, or other logic here
+	});
+})();
