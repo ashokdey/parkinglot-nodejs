340294e6784b61ba5c89d6bbb3ec83bd714bb519
ashokdey <ashokdey100@gmail.com>
2020-12-15 20:50:29 +0530

    fix: dependencies fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path';
 import logger from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import routes from './routes';
 import { v1Routes } from './routes/v1';
 
@@ -17,6 +20,9 @@ app.use(logger('dev', {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(cors());
+app.use(helmet());
 
 // Routes
 app.use('/', routes);
